58c1a12f99730b818f517bb28b2e56db23290b20
Ramez Taiyem <rameztayem@gmail.com>
2023-08-14 15:54:42 +0300

    second final commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,3 +5,6 @@ import { authOpitions } from "@/lib/session";
 const handler = NextAuth(authOpitions);
 
 export { handler as GET, handler as POST };
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+export const revalidate = 0;
--- app/api/auth/token/route.ts
+++ app/api/auth/token/route.ts
@@ -10,3 +10,6 @@ export const GET = async (req: NextRequest) => {
         console.log(error);
     }
 }
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+export const revalidate = 0;
